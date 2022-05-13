77ed68a7cb03bebb213c1eb6b30ed8c1530624ee
Jadson <jad_matheus_@hotmail.com>
2022-05-12 21:37:22 -0300

    Adicionado a lista de tarefas

--- ControleEstoque/src/br/com/controleestoque/Main.java
+++ ControleEstoque/src/br/com/controleestoque/Main.java
@@ -1,8 +1,17 @@
 package br.com.controleestoque;
 
+
+/***
+ * [] - Criar uma classe PRODUTO(Código, nome, quantidade)
+ * [] - Cadastrar produto
+ * [] - Listar produto
+ * [] - Entrada de produto
+ * [] - Saída de produto
+ * [] - Comando para encerrar o progama
+ */
 public class Main {
 
     public static void main(String[] args) {
-        
+
     }
 }
